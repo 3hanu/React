9da1ea29835d813c0afeae765bf7098412bb61e3
3hanu <72387689+3hanu@users.noreply.github.com>
2023-10-20 10:44:29 +0530

    Updates

---
+++ precache-manifest.0f719650dddb7f9399b07a926710045c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5be249e98d3a001dab495a9659fc9f8",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "9461882613ea425d2fb8",
+    "url": "/React/static/css/main.a72cb5dd.chunk.css"
+  },
+  {
+    "revision": "480e62370d4e3c250a43",
+    "url": "/React/static/js/2.72e57cf7.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/React/static/js/2.72e57cf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8bc672f7a4c0a7a534c",
+    "url": "/React/static/js/3.1da70b75.chunk.js"
+  },
+  {
+    "revision": "9461882613ea425d2fb8",
+    "url": "/React/static/js/main.7b211b8b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/React/static/js/main.7b211b8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b69a60902624180582",
+    "url": "/React/static/js/runtime-main.e418b21d.js"
+  }
+]);
